551f87b0e9a89e4d80c78034f3fc7025f25fd904
Steve Major <steve@themajorshome.com>
2017-10-11 21:11:45 -0400

    Double quotes to prevent globbing, remove ancient back ticks.

--- setadadmin.sh
+++ setadadmin.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-# version 1.0
+#!/usr/bin/env bash
+# version 1.0.1
 # This will take a group name and check if a user is a member of it against Active
 # Directory. If the user is a member, perform an action. In this case, make them a
 # local administrator on their macOS machine.
@@ -7,6 +7,6 @@
 # Set group name to check against.
 groupname="faculty"
 
-if [ "`/usr/bin/dsmemberutil checkmembership -U $@ -G $groupname`" == "user is a member of the group" ]; then
-/usr/bin/dscl . merge /Groups/admin GroupMembership $@
+if [ "$(/usr/bin/dsmemberutil checkmembership -U "$@" -G $groupname)" == "user is a member of the group" ]; then
+/usr/bin/dscl . merge /Groups/admin GroupMembership "$@"
 fi
